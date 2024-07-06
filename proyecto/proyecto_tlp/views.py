b9340c08a6fbed1fab468abc255c37a6135451ed
Patricio Pereira Ordenes <163659718+LukkyDuck1@users.noreply.github.com>
2024-07-06 19:22:14 -0400

    Rest framework y plantilla base
    Revisar rest framework. Quickstart instalado mas no funcionando realmente...

--- proyecto/proyecto_tlp/views.py
+++ proyecto/proyecto_tlp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests 
 # Create your views here.
+def home(request):
+    title = "Inicio"
+
+    data = {
+        "title" : title,
+    }
+
+    return render(request, 'proyecto_tlp/base.html',data)
