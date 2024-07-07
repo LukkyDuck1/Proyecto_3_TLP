623a32fb8ac3df36e4214e485c44a0ac31e95484
Magno Rodríguez <magno.rodriguez.cubillos@gmail.com>
2024-07-07 04:56:16 -0400

    random

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -4,11 +4,7 @@ from proyecto_tlp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('', views.home, name="home"),
-=======
-    path('home/',views.home, name="home"),
->>>>>>> 316c78d3f7d1b147afdbb23bb649a963cfc363cc
     path('api/', include('rest.urls')),
     path('registrar/', views.registrar_produccion, name='registrar_produccion'),
     path('modificar/<int:pk>/', views.modificar_produccion, name='modificar_produccion'),
