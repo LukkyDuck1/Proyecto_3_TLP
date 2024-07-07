a00a0757bb1633524c2ce7f9d38affd8244450ad
Magno Rodríguez <magno.rodriguez.cubillos@gmail.com>
2024-07-06 23:15:20 -0400

    Se agrega vista para modificar

--- proyecto/proyecto_tlp/views.py
+++ proyecto/proyecto_tlp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroProduccionForm
 from .models import RegistroProduccion
 
@@ -20,4 +20,17 @@ def registrar_produccion(request):
     else:
         form = RegistroProduccionForm()
     return render(request, 'proyecto_tlp/registro_produccion.html', {'form': form})
+
+def modificar_produccion(request, pk):
+    registro = get_object_or_404(RegistroProduccion, pk=pk)
+    if request.method == 'POST':
+        form = RegistroProduccionForm(request.POST, instance=registro)
+        if form.is_valid():
+            form.save()
+            return redirect('home') # Redirecciona a la vista home, se puede cambiar por la vista de listar produccion?
+    else:
+        form = RegistroProduccionForm(instance=registro)
+    return render(request, 'proyecto_tlp/modificar_produccion.html', {'form': form})
+
+
 
