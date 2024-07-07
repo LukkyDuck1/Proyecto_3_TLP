0920fcd33b89067adebcfeef768b94081276c819
Magno Rodríguez <magno.rodriguez.cubillos@gmail.com>
2024-07-06 22:21:42 -0400

    Se agrega vista

--- proyecto/proyecto_tlp/views.py
+++ proyecto/proyecto_tlp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import RegistroProduccionForm
+from .models import RegistroProduccion
+
 def home(request):
     title = "Inicio"
 
@@ -9,3 +10,14 @@ def home(request):
     }
 
     return render(request, 'proyecto_tlp/produccion.html',data)
+
+def registrar_produccion(request):
+    if request.method == 'POST':
+        form = RegistroProduccionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') # Redirecciona a la vista home, se puede cambiar
+    else:
+        form = RegistroProduccionForm()
+    return render(request, 'proyecto_tlp/registro_produccion.html', {'form': form})
+
